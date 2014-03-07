9d2f125a28e67fe144dd10ed638220bac3e77fb6
Peter Leonov <gojpeg@gmail.com>
2014-03-08 03:48:06 +0400

    Hash#hash, aka sub-hash as an attribute

--- serializer.rb
+++ serializer.rb
@@ -36,6 +36,21 @@ class Serializer
         attr name
       end
     end
+
+    def hash name, v=(v_empty=true), &block
+      # ask the leaf for value if v wasn't given
+      v = @leaf.send(name) if v_empty
+      
+      unless block
+        # check if the leaf is convertible to hash
+        v.respond_to? :to_h or
+          raise 'leaf is not convertible to hash (does not respond to :to_h), supply a block with appropriate rules'
+        # just return the hash representation of the leaf
+        return self[name] = v.to_h
+      end
+      
+      self[name] = self.class.new.walk(v, &block)
+    end
   end
 end
 
@@ -69,3 +84,13 @@ class HashWithManyAttrs < Serializer
   end
 end
 puts Oj.dump(HashWithManyAttrs.serialize(Person.new('John', 20))) == Oj.dump({name: "John", age: 20})
+
+class HashWithHash < Serializer
+  hash do |obj|
+    attr :name
+    hash :dog
+  end
+end
+DogOwner = Struct.new(:name, :dog)
+Dog = Struct.new(:name)
+puts Oj.dump(HashWithHash.serialize(DogOwner.new('John', Dog.new('Spike')))) == Oj.dump({name: "John", dog: {name: "Spike"}})
